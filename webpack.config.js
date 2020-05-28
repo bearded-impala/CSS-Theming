ef9e30cfb0e09d7a5a4dd44617c1e5724573a86f
Siddhesh More <beardedimpala@gmail.com>
2020-05-28 18:07:59 +0530

    publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "/CSS-Theming/",
   },
   module: {
     rules: [
